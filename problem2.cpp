2743246012df347f32e419bc4df49ab93da0fe42
jt6049 <74496902+jt6049@users.noreply.github.com>
2024-07-31 11:53:52 +0530

    problem2.cpp

--- problem2.cpp
+++ problem2.cpp
@@ -4,13 +4,13 @@ int main()
 {
     int n;
     cout << "Enter number of floors: -  ";
-    cin >> n;
-    int steps = 0;
+    cin >> n;    //Input for the number of floors
+    int steps = 0;    //variable for counting number of steps
     for (int i = 1; i <= n; i++)
     {
         if (i == 6)
             steps += 22;
-        else if (i % 2 == 0)
+        else if (i % 2 == 0)    //for checking if the floor no. is even or odd
         {
             steps += 10 * 2 + i;
         }
